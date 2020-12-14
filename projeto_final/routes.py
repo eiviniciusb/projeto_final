f6b998d3429f03529104bc5ed8cb44cbf73bf096
eiviniciusb <luiz.vinicius.batista08@aluno.ifce.edu.br>
2020-12-14 04:23:42 +0000

    atualiza projeto

--- projeto_final/routes.py
+++ projeto_final/routes.py
@@ -14,6 +14,10 @@ def login():
 def cadastro():
     return render_template('cadastro.html')
 
+@app.route('/infoad')
+def infoad():
+    return render_template('infoad.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -22,19 +26,74 @@ def contact():
 def about():
     return render_template('about.html')
 
-@app.route('/feed')
-def feed():
-    return render_template('feed.html')
+@app.route('/perfilvoluntario')
+def perfilvoluntario():
+    return render_template('perfil_voluntario.html')
+
+@app.route('/feedvoluntario')
+def feedvoluntario():
+    return render_template('feed_voluntario.html')
+
+@app.route('/anuncio')
+def anuncio():
+    return render_template('anuncio.html')
+
+@app.route('/feednaovoluntario')
+def feednaovoluntario():
+    return render_template('feed_nao_voluntario.html')
+
+@app.route('/perfilnaovoluntario')
+def perfilnaovoluntario():
+    return render_template('perfil_nao_voluntario.html')
 
 @app.route('/criaranuncio')
 def criaranuncio():
     return render_template('criaranuncio.html')
 
-@app.route('/infoad')
-def infoad():
-    return render_template('infoad.html')
+@app.route('/contsuccess')
+def contsuccess():
+    return render_template('contsuccess.html')
 
-@app.route('/resetpass')
-def resetpass():
-    return render_template('resetpass.html')
+@app.route('/logar', methods=['POST'])
+def logar():
+    email_da_pessoa = request.form['e_user']
+    senha = request.form['pass_user']
+    lembrar = request.form['lembrar']
+    return redirect('/login')
 
+@app.route('/formcadastro', methods=['POST'])
+def formcadastro():
+    nome = request.form['cad_nome']
+    sobrenome = request.form['cad_sobrenome']
+    senha = request.form['cad_senha']
+    email = request.form['cad_email']
+    cidade = request.form['cad_cidade']
+    estado = request.form['cad_estado']
+    endereco = request.form['cad_endereco']
+    complemento = request.form['cad_complemento']
+    return redirect('/infoad')
+
+@app.route('/forminfo', methods=['POST'])
+def forminfo():
+    data_nascimento = request.form['nascimento']
+    idade = request.form['idade']
+    usuario = request.form['tipo_usuario']
+    foto = request.form['foto_perfil']
+    bio = request.form['biografia']
+    return redirect('/infoad')
+
+@app.route('/formcriaranuncio', methods=['POST'])
+def formcriaranuncio():
+    titulo = request.form['titulo_anuncio']
+    imagem = request.form['imagem_anuncio']
+    email = request.form['email_contato']
+    telefone = request.form['telefone_contato']
+    return redirect('/feednaovoluntario')
+
+@app.route('/formcontact', methods=['POST'])
+def formcontact():
+    nome = request.form['cont_nome']
+    assunto = request.form['cont_assunto']
+    email = request.form['cont_email']
+    mensagem = request.form['cont_mensagem']
+    return redirect('/contsuccess')
